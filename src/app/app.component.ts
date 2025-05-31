18ab05c376c5690ec16c9b1f6fd388dc07c7b672
Your Name <mohamaduhadijatu@gmail.com>
2025-05-31 08:40:47 -0400

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-course';
+  task: string = '';
+  count= 0;
+  course = 'angular-course';
+  index:number | null = null
+  shouldUpdate:boolean = false
+  data={
+    name: 'Reese',
+    age: 24
+  }
+  Tasks:string[] = []
+  increaseCount(){
+    this.count +=1;
+  }
+  decreaseCount(){
+     this.count -=1;
+  }
+  addTask(){
+    if(this.shouldUpdate && this.index ){
+      this.Tasks.splice(this.index,1,this.task)
+      this.task = ''
+      this.shouldUpdate = false
+      this.index = null
+      
+    }
+    else{
+        if(this.task.trim() !== ''){
+      this.Tasks.push(this.task)
+    this.task = '';
+    }
+    }
+    
+  }
+  onDelete(index:number){
+    // this.Tasks = this.Tasks.filter(t => task !== t);
+    this.Tasks.splice(index,1)
+  }
+  update(index:number){
+    this.index = index
+    this.task = this.Tasks[index]
+    this.shouldUpdate = true
+  }
 }
